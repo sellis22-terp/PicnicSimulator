92b86525cbc89ff08d11aa6315c6eb90b683f680
Avy4 <104154099+Avy4@users.noreply.github.com>
2026-03-25 18:26:29 -0400

    Changes 3/25/26
    - Refactored assets a little bit to make the folder cleaner
    - Finished dialogue system

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,19 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Dialogue : MonoBehaviour
-{
+{   
+    // I used a Queue cause its annoying to keep track of an index to go thru the dialogue
+    // I couldnt make it a SerializeField though cause Unity doesn't let you serialize Queues
+    private Queue<string> dialogue;
+    [SerializeField] string[] dialogueTemp;
+    [SerializeField] TextMeshProUGUI dialogueText;
 
-    public string[] dialogue;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    {   
+        // Convert the Array into a Queue
+        dialogue = new Queue<string>(dialogueTemp);
+        SetText();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetText()
+    {   
+        // Makes sure theres still stuff in the queue before it tries to grab it
+        if (dialogue.Count != 0) {
+            dialogueText.SetText(dialogue.Dequeue());
+        }
     }
 }
---
+++ Assets/Scripts/LittleSquirrel.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LittleSquirrel : MonoBehaviour
+{
+    
+    [SerializeField] GameObject dialogueGUI;
+    [SerializeField] PlayerInput input;
+
+    void Start()
+    {   
+        // Turn it off to start, I also did it in the scene view so this is kinda redundant
+        dialogueGUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // Just in case
+        if (other.tag == "Player")
+        {   
+            // Visible   
+            dialogueGUI.SetActive(true);
+        }
+
+        // Without this whenever you try to move your mouse to the next button the screen would move
+        // This line essentially disables OnLook in ThirdPersonControls.cs
+        input.actions.FindAction("Look").Disable();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {   
+            // Invisible
+            dialogueGUI.SetActive(false);
+        }
+
+        // Renables
+        input.actions.FindAction("Look").Enable();
+    }
+}
